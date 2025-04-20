cfd6a76b0aefcc6ee6ce5552deb48b14153ec8a1
Saskia Rey <saskia.rey96@gmail.com>
2025-04-20 13:12:05 +0200

    Implemented password encrypting

--- src/main/java/rey/SpringSecurityDemo/service/UserService.java
+++ src/main/java/rey/SpringSecurityDemo/service/UserService.java
@@ -1,6 +1,7 @@
 package rey.SpringSecurityDemo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import rey.SpringSecurityDemo.model.Users;
 import rey.SpringSecurityDemo.repo.UserRepo;
@@ -9,7 +10,9 @@ import rey.SpringSecurityDemo.repo.UserRepo;
 public class UserService {
     @Autowired
     private UserRepo repo;
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
     public Users register(Users user){
+        user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
     }
 }
