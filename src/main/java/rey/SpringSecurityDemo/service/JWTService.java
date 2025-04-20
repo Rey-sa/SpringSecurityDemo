bf877c948a706db9b5fbcea07d8d386ff13d7e44
Saskia Rey <saskia.rey96@gmail.com>
2025-04-20 17:15:39 +0200

    Implemented generating JWT Token

---
+++ src/main/java/rey/SpringSecurityDemo/service/JWTService.java
@@ -0,0 +1,50 @@
+package rey.SpringSecurityDemo.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JWTService {
+    private String secretKey = "";
+
+    public JWTService() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey sk = keyGen.generateKey();
+            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public String generateToken(String username){
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(Date.from(Instant.now()))
+                .expiration(Date.from(Instant.now().plus(Duration.ofHours(1))))
+                .and()
+                .signWith(getKey())
+                .compact();
+    }
+
+    private Key getKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
--- src/main/java/rey/SpringSecurityDemo/service/UserService.java
+++ src/main/java/rey/SpringSecurityDemo/service/UserService.java
@@ -1,6 +1,9 @@
 package rey.SpringSecurityDemo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import rey.SpringSecurityDemo.model.Users;
@@ -9,10 +12,24 @@ import rey.SpringSecurityDemo.repo.UserRepo;
 @Service
 public class UserService {
     @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
     private UserRepo repo;
+
+    @Autowired
+    private JWTService jwtService;
+
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
+
     public Users register(Users user){
         user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
+    }
+
+    public String verify(Users user) {
+        Authentication authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        return authentication.isAuthenticated()? jwtService.generateToken(user.getUsername()) : "Failed";
     }
 }
